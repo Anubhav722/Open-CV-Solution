89ca0b6794cbf2ae2980bc80272328b02d99603c
Anubhav722 <anubhavs286@gmail.com>
2017-05-21 13:35:54 +0530

    camera filters

--- changing_colorspaces/flags_available_for_color_conversion.py
+++ changing_colorspaces/flags_available_for_color_conversion.py
@@ -1,3 +1,11 @@
 import cv2
 flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
 print flags
+
+# FOR MORE INFO SEE: 
+# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html#converting-colorspaces
+
+
+# FOR COLOR CONVERSION:
+# cv2.cvtColor(input_image, flag)
+# where flag determines the type of conversion
